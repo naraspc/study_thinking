4357174d14676d75bf3420e100fd028ae663f516
naraspc <wodnrtkfka@naver.com>
2023-08-05 22:41:08 +0900

    feat Add Update in ArticleApiController
    외부에서 전달받은 Json 데이터를 DB에 있는 기존 데이터와 비교,
    ID가 같다면 수정함

--- src/main/java/com/example/JWProject/api/ArticleApiController.java
+++ src/main/java/com/example/JWProject/api/ArticleApiController.java
@@ -3,26 +3,33 @@ package com.example.JWProject.api;
 import com.example.JWProject.DTO.ArticleForm;
 import com.example.JWProject.Repository.ArticleRepository;
 import com.example.JWProject.entity.Article;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@Slf4j
+@ResponseStatus
 @RestController // RestAPI 컨트롤러 (JSON반환)
 public class ArticleApiController {
     @Autowired // 외부에서 가져오기
     private ArticleRepository articleRepository;
+
     //GET
     @GetMapping("/api/articles") // 모든 객체 반환
     public List<Article> index() {
         return articleRepository.findAll();
 
     }
+
     @GetMapping("/api/articles/{id}") // 단일 객체 반환
     public Article index(@PathVariable Long id) {
         return articleRepository.findById(id).orElse(null);
 
     }
+
     //POST
     @PostMapping("/api/articles")
     public Article Create(@RequestBody ArticleForm dto) { // 전달받은 Json데이터를 DTO로 변환
@@ -32,6 +39,24 @@ public class ArticleApiController {
 
     //PATCH
 
+    @PatchMapping("/api/articles/{id}")
+    public ResponseEntity<Article> update(@PathVariable Long id, @RequestBody ArticleForm dto) {
+
+       // 수정용 엔티티 생성
+        Article article = dto.toEntity();
+        log.info("id: {}, article: {}", id, article.toString());
+
+        // 대상 엔티티 조회
+        Article target = articleRepository.findById(id).orElse(null);
+
+        // 잘못된 요청 처리(대상이 없거나 id가 다른경우)
+        if (target == null | article.getId() != id ){
+            log.info("잘못된 요청 ! id: {}, article: {}", id, article.toString());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+        Article updated = articleRepository.save(article);
+        return ResponseEntity.status(HttpStatus.OK).body(updated);
+    }
     //DELETE
 
 }
